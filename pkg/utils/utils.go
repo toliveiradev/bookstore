3eae887a5d5a443c3c4c2725d81e042840171158
toliveiradev <tiagojavapro@gmail.com>
2024-02-26 19:35:49 -0300

    hotfix: controller method name and return for utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -9,7 +9,8 @@ import (
 func ParseBody(r *http.Request, model interface{}) error {
 	if body, err := io.ReadAll(r.Body); err == nil {
 		if err := json.Unmarshal([]byte(body), model); err != nil {
-			return
+			return err
 		}
 	}
+	return nil
 }
